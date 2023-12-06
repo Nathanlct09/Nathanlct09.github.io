141598c656b7d006037377ff05bd65c67aa3f295
Nathan <nathan.lecomte@epitech.eu>
2023-12-06 22:39:12 +0100

    pitie 5

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/https://github.com/Nathanlct09/Nathanlct09.github.io/',
   plugins: [vue()],
 })
