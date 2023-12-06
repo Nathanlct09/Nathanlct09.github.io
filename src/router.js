927b6935c600782b33263bb997fb98b6a21038aa
Nathan <nathan.lecomte@epitech.eu>
2023-12-06 21:17:12 +0100

    pitie 4

---
+++ src/router.js
@@ -0,0 +1,8 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import App from '../src/App.vue';
+import Login from './components/Login.vue';
+import Home from'./Home.vue';
+import Registration from'./components/Registration.vue';
+import Profile from'./components/Profile.vue';
+import Accueil from'./components/Accueil.vue';
+import AdminChart from './components/AdminChart.vue';
